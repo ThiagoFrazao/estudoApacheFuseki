dd79c0ea50918ff553d9bf4cbe4c6000e777e53e
Thiago Frazão de Sousa <thiago_frasao@hotmail.com.br>
2018-08-30 23:54:39 -0300

    Estudando ontologias

--- apache-jena/src/main/java/jena/Ontologia.java
+++ apache-jena/src/main/java/jena/Ontologia.java
@@ -1,5 +1,25 @@
 package jena;
 
+import java.util.ArrayList;
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntProperty;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.util.iterator.ExtendedIterator;
+
 public class Ontologia {
+	
+	public static void main(String[] args) {
+		OntModel ont = ModelFactory.createOntologyModel();
+		ont.read("C:/Users/thiag/Documents/Faculdade/TCC/Ontologias/ModeloRDF-XML.owl", "RDF/XML");
+		ExtendedIterator<OntProperty> ei = ont.listAllOntProperties();
+		ArrayList<Property> props = new ArrayList<Property>();
+		
+		while(ei.hasNext()){
+			Property prop = ont.getProperty(ei.next().toString());
+			System.out.println(prop.getURI());
+		}
+	}
 
 }
